1e072456b3cf924005f2b704879087075640c619
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2018-10-13 18:25:16 -0700

    Another bug fix

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   alert("page is loaded and ready");
-  
-  function changeParagraphContent() {
-    var element = document.getElementById("demo");
-    if element.classList.contains("red") {
-      element.innerHTML = "This page contains some simple html and css";
-      element.classList.remove("red");
-    } else {
-      element.innerHTML = "This is the new paragraph text";
-      element.classList.add("red");
-    }
-  }
 });
+
+ 
+function changeParagraphContent() {
+  var element = document.getElementById("demo");
+  if(element.classList.contains("red")) {
+    element.innerHTML = "This page contains some simple html and css";
+    element.classList.remove("red");
+  } else {
+    element.innerHTML = "This is the new paragraph text";
+    element.classList.add("red");
+  }
+}
